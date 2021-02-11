a48dd77e27ce8f2fae20fe1ef26b65768def7a50
Richard Pinegar <rich.pinegar@gmail.com>
2021-02-11 09:23:21 -0500

    create routines view, stylesheet, add icons

---
+++ routines.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <!-- Main stylesheet -->
+    <link rel="stylesheet" href="style/style.css">
+
+    <title>mySpotter</title>
+  </head>
+  <body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-2 col1">
+                <img src="icons/filter-left.svg" alt="dropdown-menu" />
+            </div>
+            <div class="col-8 col2">
+                <h4 class="text-primary">mySpotter</h4>
+            </div>
+            <div class="col-2 col3">
+                <a class="add-routine" href="#">
+                <img src="icons/plus-circle-fill.svg" alt="add-routine" />
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h1 class="header">Routines</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h6 class="sub-header">4 Workouts</h6>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <a class="task" href="#">
+                    <i class="task-header">Week 1</i>
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <a class="task" href="#">
+                    <i class="task-header">Week 2</i>
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <a class="task" href="#">
+                    <i class="task-header">Week 3</i>
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <a class="task" href="#">
+                    <i class="task-header">Week 4</i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+  </body>
+</html>
